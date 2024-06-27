1dda7841ab1b5348d53474e5337f52d9df280a1d
dinaaz.shaikh <dinaaz.shaikh@talabat.com>
2024-06-28 02:55:26 +0400

    add test

---
+++ csharp/dotnet6/DotnetStarter.Logic.Tests/MultiplyTest.cs
@@ -0,0 +1,43 @@
+using Xunit;
+
+namespace DotnetStarter.Logic.Tests;
+
+/**
+ * Test class for Multiply
+ * 1) two positive numbers multiplication
+ * 2) multiply by 0
+ * 3) multiply by 1
+ * 4) multiply by negative number
+ * 5) one positive and one negative number multiplication
+ * 6) two negative numbers multiplication
+ */
+public class MultiplyTest
+{
+
+    [Fact]
+    public void Multiply_ByZero()
+    {
+
+        var multiply = new Multiply(0,3);
+        var result = multiply.process();
+        
+        Assert.Equal(0, result);
+    }
+}
+
+public class Multiply
+{
+    private readonly int num1;
+    private readonly int num2;
+
+    public Multiply(int num1, int num2)
+    {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public int process()
+    {
+        return 0;
+    }
+}
