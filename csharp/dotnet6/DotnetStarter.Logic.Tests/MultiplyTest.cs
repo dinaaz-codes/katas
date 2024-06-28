cf73ad319da747df0605e679892d1737f6d0fd8f
dinaaz.shaikh <dinaaz.shaikh@talabat.com>
2024-06-28 13:43:29 +0400

    xm

--- csharp/dotnet6/DotnetStarter.Logic.Tests/MultiplyTest.cs
+++ csharp/dotnet6/DotnetStarter.Logic.Tests/MultiplyTest.cs
@@ -64,12 +64,17 @@ public class Multiply
             return num1;
 
         var result = 0;
-        for (int i = 0; i < num1; i++)
+        for (int i = 0; IsLessThan(i); i++)
         {
             result += num2;
         }
 
         return result;
+    }
+
+    private bool IsLessThan(int i)
+    {
+        return i < num1;
     }
 
     private static bool IsNumberOne(int num)
