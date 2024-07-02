5f0da8ea14da5fdc6132710952d7afa268b417e7
dinaaz.shaikh <dinaaz.codes@gmail.com>
2024-07-02 18:27:59 +0400

    add test

---
+++ csharp/dotnet6/DotnetStarter.Logic.Tests/MarsRoverTest.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace DotnetStarter.Logic.Tests;
+
+/*
+ * ShouldMoveForward
+ * ShouldMoveBackword
+ * ShouldMoveLeft
+ * ShouldMoveRight
+ */
+public class MarsRoverTest
+{
+
+ [Fact]
+ public void ShouldMoveForward()
+ {
+     var marsRover = new MarsRover();
+
+     marsRover.Move(new List<char>['f']);
+     
+     Assert.Equal(1,marsRover.x);
+    Assert.Equal(0,marsRover.y);
+     
+ }
+}
+
+public class MarsRover
+{
+    public int x;
+    public int y;
+
+    public void Move(List<char>[] lists)
+    {
+        x = x + 1;
+    }
+}
