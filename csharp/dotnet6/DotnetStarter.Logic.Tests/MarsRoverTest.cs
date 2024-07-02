4537a1d63410da4caafcf87a797eb2d1f6607cce
dinaaz.shaikh <dinaaz.codes@gmail.com>
2024-07-02 18:35:46 +0400

    add test

--- csharp/dotnet6/DotnetStarter.Logic.Tests/MarsRoverTest.cs
+++ csharp/dotnet6/DotnetStarter.Logic.Tests/MarsRoverTest.cs
@@ -15,12 +15,28 @@ public class MarsRoverTest
     [Fact]
     public void ShouldMoveForward()
     {
-        var marsRover = new MarsRover();
+        var marsRover = new MarsRover(0,0);
 
-        marsRover.Move(new List<char>['f']);
+        marsRover.Move(new List<char>()
+        {
+            'f'
+        });
 
         Assert.Equal(1, marsRover.x);
         Assert.Equal(0, marsRover.y);
+    }
+    [Fact]
+    public void ShouldMoveBackward()
+    {
+        var marsRover = new MarsRover(0,0);
+
+        marsRover.Move(new List<char>()
+        {
+            'b'
+        });
+
+        Assert.Equal(0, marsRover.x);
+        Assert.Equal(-1, marsRover.y);
     }
 }
 
@@ -29,8 +45,25 @@ public class MarsRover
     public int x;
     public int y;
 
-    public void Move(List<char>[] lists)
+    public MarsRover(int x, int y)
     {
-        x = x + 1;
+        this.x = x;
+        this.y = y;
+    }
+
+    public void Move(List<char> lists)
+    {
+        foreach (var command in lists)
+        {
+            if (command == 'f')
+            {
+                x = x + 1;
+            }
+
+            if (command == 'b')
+            {
+                y = y - 1;
+            }
+        }
     }
 }
